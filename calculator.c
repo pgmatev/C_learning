b2d2696ad8ba454a492b77e13631576b6cd1e70e
pgmatev <petargabriel.matev@gmail.com>
2017-12-14 12:26:19 +0200

    added calculator

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int menu;
+    float num1, num2, result;
+
+    printf("Enter a number from the list below\n\n");
+
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n\n");
+
+
+}
+
+
+
