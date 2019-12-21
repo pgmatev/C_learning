ade5d2cc8a4b87c1e9caa3f7057c970d1c9b96fa
pgmatev <petargabriel.matev@gmail.com>
2019-12-21 22:30:13 +0200

    shell

--- main.c
+++ main.c
@@ -1,138 +1,52 @@
-#include <stdio.h>
+//--------------------------------------------
+// NAME: Petar-Gabriel Matev
+// CLASS: XIb
+// NUMBER: 22
+// PROBLEM: #2
+// FILE NAME: main.c
+// FILE PURPOSE:
+// shell
+//---------------------------------------------
+
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv)
 {
-    int _31,_30,_28,d,m,y,d1,m1,y1,leap=0;
+	int init_size = strlen(argv);
+	char delim[] = " ";
 
+	char *ptr = strtok(argv, delim);
 
-    do{
-        printf("Year: ");
-        scanf("%d",&y);
-    }while(y<=0);
+    pid_t pid;
+    int status;
 
-    if(y%4==0 && y%100!=0)
-        leap=1;
-
-    if(y%100==0 && y%400==0)
-        leap=1;
-    if(y%4!=0)
-        leap=0;
-
-
-    do{
-        printf("Month: ");
-        scanf("%d",&m);
-    }while(m<=0 || m>12);
-    if(m==2){
-        _28=1;
-        _30=0;
-        _31=0;
-    }
-    if(m==4 || m==6 || m==9 || m==11){
-        _28=0;
-        _30=1;
-        _31=0;
-    }
-    if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12){
-        _28=0;
-        _30=0;
-        _31=1;
-    }
-    if(_28==1){
-        if(leap==0){
-            do{
-                printf("Day: ");
-                scanf("%d",&d);
-            }while(d<=0 || d>28);
-        }
-        else{
-            do{
-                printf("Day: ");
-                scanf("%d",&d);
-            }while(d<=0 || d>29);
-        }
-    }
-    if(_30==1){
-        do{
-            printf("Day: ");
-            scanf("%d",&d);
-        }while(d<=0 || d>30);
-    }
-    if(_31==1){
-        do{
-            printf("Day: ");
-            scanf("%d",&d);
-        }while(d<=0 || d>31);
-    }
-    d1=d;
-    m1=m;
-    y1=y;
-    if(d==1 && m!=1){
-        m-=1;
-        if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12)
-            d=31;
-        if(m==4 || m==6 || m==9 || m==11)
-            d=30;
-        if(m==2){
-            if(leap==1)
-                d=29;
-            else
-                d=28;
-        }
-    }
-    if(m!=1 && d!=1)
-        d-=1;
-    if(m==1 && d==1){
-        y-=1;
-        m=12;
-        d=31;
-    }
-    printf("\nYesterday was %d.%d.%d\n",d,m,y);
-    if(m!=12){
-      if(_31==1 && d1==31){
-          m1+=1;
-          d1=1;
-        }
-      if(_30==1 && d1==30){
-          m1+=1;
-          d1=1;
-        }
-      if(_28==1){
-          if(leap==0 && d1==28){
-            m1+=1;
-            d1=1;
-
-          }
-      if(leap==1 && d1==29){
-              m1+=1;
-              d1=1;
+	while(ptr != NULL)
+	{
+        while(1){
+            pid = fork();
+            if(pid == 0){
+                if(execl(ptr, ptr+1) != 0){
+                    perror(ptr);
+                    return -1;
+                }
+            }   
+            else if(pid == -1) {
+                perror("fork");
+            } else {
+                if(waitpid(pid, &status, 0) != pid) {
+                    perror("");
+                    return -1;
+                }
+                sleep(2);
             }
-          }
-      if(_31==1 && d1!=31){
-        d1+=1;
-      }
-      if(_30==1 && d1!=30){
-        d1+=1;
-      }
-      if(_28==1){
-        if(leap==0 && d1!=28){
-          d1+=1;
         }
-        if(leap==1 && d1!=29){
-          d1+=1;
-        }
-      }
-    }
-      if(m1==12 && d1!=31){
-          d1++;
-        }
-        if(m1==12 && d1==31){
-          y1+=1;
-          m1=1;
-          d1=1;
-        }
-            printf("Tomorrow is %d.%d.%d\n",d1,m1,y1);
-            return 0;
-
+		ptr = strtok(NULL, delim);
+	}
+	return 0;
 }
